38f24581cd47412b1824cfbb8e78e48f77d82d3f
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2015-07-21 15:59:33 +0200

    Undefined bug

--- src/frontend/load_mbvh.cpp
+++ src/frontend/load_mbvh.cpp
@@ -28,6 +28,7 @@ struct node {
 
 extern "C" {
     void put_int(int i) { printf("%d\n", i); fflush(stdout); }
+    void put_float(float f) { printf("%f\n", f); fflush(stdout); }
     void puts_flush(const char* c) { puts(c); fflush(stdout); }
 }
 
@@ -51,12 +52,11 @@ bool load_accel(const std::string& filename, Node*& nodes_ref, Vec4*& tris_ref) 
     std::vector<Node> node_stack;
     std::vector<Vec4> tri_stack;
     union { unsigned int i; float f; } sentinel = { 0x80000000 };
-    Vec4 tri_sentinel = { sentinel.f, sentinel.f, sentinel.f, sentinel.f };
 
     auto leaf_node = [&] (const mbvh::node& node, int c) {
         int node_id = ~(tri_stack.size());
         for (int i = 0; i < node.prim_count[c]; i++) {
-            int i0 = i + node.children[c], i1 = i0 + 1, i2 = i0 + 2;
+            int i0 = node.children[c] + i * 3, i1 = i0 + 1, i2 = i0 + 2;
             Vec4 v0 = { vertices[i0 * 3 + 0], vertices[i0 * 3 + 1], vertices[i0 * 3 + 2], 0.0f };
             Vec4 v1 = { vertices[i1 * 3 + 0], vertices[i1 * 3 + 1], vertices[i1 * 3 + 2], 0.0f };
             Vec4 v2 = { vertices[i2 * 3 + 0], vertices[i2 * 3 + 1], vertices[i2 * 3 + 2], 0.0f };
@@ -64,7 +64,7 @@ bool load_accel(const std::string& filename, Node*& nodes_ref, Vec4*& tris_ref) 
             tri_stack.push_back(v1);
             tri_stack.push_back(v2);
         }
-        tri_stack.push_back(tri_sentinel);
+        tri_stack.back().w = sentinel.f;
         return node_id;
     };
 
@@ -96,7 +96,25 @@ bool load_accel(const std::string& filename, Node*& nodes_ref, Vec4*& tris_ref) 
 
             k++;
         }
-        if (k < 4) { dst_node.children[k] = 0; }
+
+        for (; k < 4; k++) {
+            dst_node.min_x[k] = 1.0f;
+            dst_node.min_y[k] = 1.0f;
+            dst_node.min_z[k] = 1.0f;
+
+            dst_node.max_x[k] = -1.0f;
+            dst_node.max_y[k] = -1.0f;
+            dst_node.max_z[k] = -1.0f;
+
+            dst_node.children[k] = 0;
+        }
+
+        for (int c = 0; c < 4; c++) {
+            if (dst_node.children[c] > 0 && dst_node.children[c] < i) {
+                printf("BUG\n");
+            }
+        }
+
         node_stack.push_back(dst_node);
     }
 
