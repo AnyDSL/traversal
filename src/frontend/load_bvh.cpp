82767a8a9857338cce8b1597c0e2f18d62167c8a
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2015-11-02 15:24:18 +0100

    Compile fix in GPU version

--- src/frontend/load_bvh.cpp
+++ src/frontend/load_bvh.cpp
@@ -14,7 +14,7 @@ static inline float as_float(int i) {
     return u.f;
 }
 
-bool load_accel(const std::string& filename, Node*& nodes_ref, Vec4*& tris_ref) {
+bool load_accel(const std::string& filename, thorin::Array<Node>& nodes_ref, thorin::Array<Vec4>& tris_ref) {
     std::ifstream in(filename, std::ifstream::binary);
     if (!in || !check_header(in) || !locate_block(in, BlockType::BVH))
         return false;
